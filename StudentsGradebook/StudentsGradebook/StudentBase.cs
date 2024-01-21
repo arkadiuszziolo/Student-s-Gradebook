6a534ae6dc38ebaf1612788b0e428e879c2706c4
Arek Zioło <arek_z77@onet.eu>
2024-01-21 14:59:59 +0100

    Add method which were in other files

--- StudentsGradebook/StudentsGradebook/StudentBase.cs
+++ StudentsGradebook/StudentsGradebook/StudentBase.cs
@@ -21,13 +21,108 @@
 
         public abstract void AddGrade(float grade);
 
-        public abstract void AddGrade(int grade);
+        public void AddGrade(int grade)
+        {
+            float intToFloat = grade;
+            this.AddGrade(intToFloat);
+        }
 
-        public abstract void AddGrade(string grade);
+        public void AddGrade(string grade)
+        {
+            switch (grade)
+            {
+                case "A":
+                case "6":
+                    this.AddGrade(6);
+                    break;
+                case "A-":
+                case "-A":
+                case "-6":
+                case "6-":
+                    this.AddGrade(5.75f);
+                    break;
+                case "B+":
+                case "+B":
+                case "+5":
+                case "5+":
+                    this.AddGrade(5.50f);
+                    break;
+                case "B":
+                case "5":
+                    this.AddGrade(5);
+                    break;
+                case "B-":
+                case "-B":
+                case "-5":
+                case "5-":
+                    this.AddGrade(4.75f);
+                    break;
+                case "C+":
+                case "+C":
+                case "+4":
+                case "4+":
+                    this.AddGrade(4.50f);
+                    break;
+                case "C":
+                case "4":
+                    this.AddGrade(4);
+                    break;
+                case "C-":
+                case "-C":
+                case "-4":
+                case "4-":
+                    this.AddGrade(3.75f);
+                    break;
+                case "D+":
+                case "+D":
+                case "+3":
+                case "3+":
+                    this.AddGrade(3.50f);
+                    break;
+                case "D":
+                case "3":
+                    this.AddGrade(3);
+                    break;
+                case "D-":
+                case "-D":
+                case "-3":
+                case "3-":
+                    this.AddGrade(2.75f);
+                    break;
+                case "E+":
+                case "+E":
+                case "+2":
+                case "2+":
+                    this.AddGrade(2.50f);
+                    break;
+                case "E":
+                case "2":
+                    this.AddGrade(2);
+                    break;
+                case "E-":
+                case "-E":
+                case "-2":
+                case "2-":
+                    this.AddGrade(1.75f);
+                    break;
+                case "F+":
+                case "+F":
+                case "+1":
+                case "1+":
+                    this.AddGrade(1.50f);
+                    break;
+                case "F":
+                case "1":
+                    this.AddGrade(1);
+                    break;
+                default:
+                    Console.WriteLine($"Invalid String: {grade}");
+                    break;
+            }
+        }
 
         public abstract void GetAllValuesFromList();
 
         public abstract Grades GetGrades();
-
     }
 }
